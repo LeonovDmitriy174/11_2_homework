e0c7f5004ca0eb74daf65585a31380b7a9ccc5d0
Дмитрий Леонов <dmitriy.leonov.1993@mail.ru>
2023-12-30 09:49:40 +0500

    docstring with func1 and add func2 in str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def function(value):
+    '''Функция, возвращающая строку со всему заглавными буквами'''
     return value.upper()
+
+
+def new_function(value):
+    '''Функция, возвращающая строку заглавными первыми буквами каждого слова'''
+    return value.title()
